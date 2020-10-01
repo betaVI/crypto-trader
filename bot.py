c75b73c728cb141621242dda03a999ecbcaed6bd
Brian Graham <bmg224@outlook.com>
2020-10-01 08:02:41 -0700

    implemented authenticated client

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import sys
 import signal
 import time
-from libs.public_client import PublicClient
+from libs.authenticated_client import AuthenticatedClient
 
 def handle_cancel(sig, frame):
     print('Cancelled')
@@ -11,16 +11,22 @@ signal.signal(signal.SIGINT, handle_cancel)
 
 #configuration values
 runinterval = 30
+key = 'xxxxxxxxxxxxxxxxx'
+b64secret = 'xxxxxxxxxxxxxxxxxxxxxx=='
+passphrase = 'xxxxxxxxxxxx'
 url = 'https://api-public.sandbox.pro.coinbase.com'
 #product_id = 'LTC-USD'
 product_id = 'BTC-USD'
 
-publicClient = PublicClient(api_url=url)
+authClient = AuthenticatedClient(key, b64secret, passphrase, api_url=url)
 
 while True:
     print('Start')
 
-    results = publicClient.get_product_ticker(product_id=product_id)
+    results = authClient.get_product_ticker(product_id=product_id)
+    print(results)
+
+    results = authClient.get_accounts()
     print(results)
 
     print('End')
