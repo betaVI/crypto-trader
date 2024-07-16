c4d8925b5e8dc723eca1d8622c542a2b76bc888a
Brian Graham <brianmgraham224@gmail.com>
2024-07-15 21:29:44 -0700

    added timer thread to run truncate logs

--- src/bots/databot.py
+++ src/bots/databot.py
@@ -1,4 +1,4 @@
-import os, sys, time, signal, logging
+import os, sys, time, signal, logging, threading
 from dependency_injector.wiring import Provide, inject
 from src.bots.dbloghandler import DbLogHandler
 from src.container import Container, create_container
@@ -9,30 +9,23 @@ from src.data.traderrepository import TraderRepository
 from src.data.logsrepository import LogsRepository
 from src.data.ordersrepository import OrdersRepository
 
+truncate_logs_timer : threading.Timer = None
+
 def handle_cancel(sig, frame):
     print('Cancelled')
+    if truncate_logs_timer:
+        truncate_logs_timer.cancel()
+        print('Timer Cancelled')
     sys.exit(0)
 
-@inject
-def runapp(
-    api: CbApi = Provide[Container.cbapi_client],
-    db: DataAccess = Provide[Container.db],
-    logrepo: LogsRepository = Provide[Container.logs_repo],
-    traderrepo: TraderRepository = Provide[Container.traders_repo],
-    orderrepo: OrdersRepository = Provide[Container.orders_repo],
-    log_interval: int = Provide[Container.config.logretention.interval.as_(int)],
-    log_frequency: str = Provide[Container.config.logretention.frequency]
-    ):
+def truncate_logs(logrepo: LogsRepository, log_interval: int, log_frequency: str):
+    utillogger = logging.getLogger('UTIL')
+    utillogger.setLevel(logging.DEBUG)
+    utillogger.debug('Removing Logs older than {} {}'.format(log_interval, log_frequency))
+    logrepo.purgeLogs(log_interval, log_frequency)
+    utillogger.debug('Removed Logs older than {} {}'.format(log_interval, log_frequency))
 
-    loghandler = DbLogHandler(logrepo)
-    console = logging.StreamHandler()
-    console.setFormatter(logging.Formatter(fmt='%(asctime)s | %(levelname)-8s | %(name)-8s | %(message)s', datefmt='%m-%d-%Y %H:%M:%S'))
-    logging.getLogger('').addHandler(loghandler)
-    logging.getLogger('').addHandler(console)
-
-    print('cwd is %s' %(os.getcwd()))
-    signal.signal(signal.SIGINT, handle_cancel)
-
+def runbots(db: DataAccess, api: CbApi, traderrepo: TraderRepository, orderrepo: OrdersRepository):
     while True:
         settings = db.executeScalar("SELECT * FROM settings")
         runinterval = int(settings['interval'])
@@ -51,11 +44,34 @@ def runapp(
                 traderrepo.deleteTrader(int(traderconfig['id']))
         
         botlogger.debug('Waiting ' + str(runinterval) + ' seconds')
-        botlogger.debug('Removing Logs older than {} {}'.format(log_interval, log_frequency))
-        logrepo.purgeLogs(log_interval, log_frequency)
-        botlogger.debug('Removed Logs older than {} {}'.format(log_interval, log_frequency))
 
         time.sleep(runinterval)
+
+@inject
+def runapp(
+    api: CbApi = Provide[Container.cbapi_client],
+    db: DataAccess = Provide[Container.db],
+    logrepo: LogsRepository = Provide[Container.logs_repo],
+    traderrepo: TraderRepository = Provide[Container.traders_repo],
+    orderrepo: OrdersRepository = Provide[Container.orders_repo],
+    log_interval: int = Provide[Container.config.logretention.interval.as_(int)],
+    log_frequency: str = Provide[Container.config.logretention.frequency]
+    ):
+
+    loghandler = DbLogHandler(logrepo)
+    console = logging.StreamHandler()
+    console.setFormatter(logging.Formatter(fmt='%(asctime)s | %(levelname)-8s | %(name)-8s | %(message)s', datefmt='%m-%d-%Y %H:%M:%S'))
+    logging.getLogger('').addHandler(loghandler)
+    logging.getLogger('').addHandler(console)
+
+    signal.signal(signal.SIGINT, handle_cancel)
+
+    #start truncate_logs timer process to remove old logs
+    truncate_logs_timer = threading.Timer(interval=30.0, function=truncate_logs, args=(logrepo, log_interval, log_frequency))
+    truncate_logs_timer.start()
+
+    #start main process
+    runbots(db,api, traderrepo, orderrepo)
 
 if __name__ == "__main__":
     create_container(__name__)
